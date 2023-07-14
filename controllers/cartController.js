956a321d006a772eead300853e48bb7c02c52485
Nahuel Venerus <nahuelvenerus@gmail.com>
2023-07-14 16:51:37 -0300

    feat: purchase cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,6 +1,13 @@
 const asyncHandler = require("express-async-handler");
 const { searchUser } = require("../services/userServices");
-const { getAllCarts, getUserCart } = require("../services/cartServices");
+const {
+  getAllCarts,
+  getUserCart,
+  purchaseCart,
+} = require("../services/cartServices");
+const {
+  purchaseCart,
+} = require("../../geek-central-front/src/services/cart/purchaseCart");
 
 exports.get_all_carts = asyncHandler(async (req, res) => {
   try {
@@ -16,6 +23,18 @@ exports.get_user_cart = asyncHandler(async (req, res) => {
     const { nickname } = req.params;
     const user = await searchUser(nickname);
     let userShoppingCart = await getUserCart(user.id);
+    res.status(200).send(userShoppingCart);
+  } catch (error) {
+    throw Error(error);
+  }
+});
+
+exports.purchase_user_cart = asyncHandler(async (req, res) => {
+  try {
+    const { nickname } = req.params;
+    const user = await searchUser(nickname);
+    let userShoppingCart = await getUserCart(user.id);
+    await purchaseCart(userShoppingCart.id);
     res.status(200).send(userShoppingCart);
   } catch (error) {
     throw Error(error);
--- routes/Carts.js
+++ routes/Carts.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   get_user_cart,
   get_all_carts,
+  purchase_user_cart,
 } = require("../controllers/cartController");
 
 router.get("/", get_all_carts);
 router.get("/:nickname", get_user_cart);
+router.put("/purchase/:nickname", purchase_user_cart);
 
 module.exports = router;
