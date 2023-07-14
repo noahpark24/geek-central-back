ee0042773707ad540ba9fa2c630b76373db2e0b6
ivan <ivan.correa24@yahoo.com>
2023-07-14 14:57:14 -0300

    bug-fix:model user

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.use(express.json());
 app.use("/api", router);
 
 //config
-db.sync({ force: true }).then(() =>
+db.sync({ force: false }).then(() =>
   app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
 );
